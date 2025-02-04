5c8cacdcdd364e8e38557ac7e1adb9a120bdb81d
水咲(みさき) <60120497+Misaki0331@users.noreply.github.com>
2025-02-04 17:20:53 +0900

    feat: クライアントサイドのHTMLの作成 (#1)

---
+++ html/items.js
@@ -0,0 +1,79 @@
+let previousItemData = [];
+const freq = new URLSearchParams(window.location.search).get('freq');
+// ロケールによって表示するテキストを変更する
+window.addEventListener("load", function () {
+    document.getElementById('item-info-title').innerText = localeText[locale].itemDetailInfoTitle;
+    document.getElementById('queueId-header').innerText = localeText[locale].queueIdHeader;
+    document.getElementById('name-header').innerText = localeText[locale].nameHeader;
+    document.getElementById('amount-header').innerText = localeText[locale].amountHeader;
+
+    const f = freq.toLocaleLowerCase();
+    const ids = f.split(',').map(id => `<div class="freq ${id}"></div>`).join('');
+    const text = f.split(',').map(id => localeColour[locale][id] || id).join(', ');
+    document.getElementById('channel-title').innerHTML = ids + `<p class="txt freq-guide">${text}</p>`;;
+});
+
+
+async function fetchItem() {
+    const url = new URL('/api/list/items.json', window.location.origin);
+    url.searchParams.set('frequency', freq);
+    const response = await fetch(url);
+    const data = await response.json();
+    const table = document.getElementById('item-list').getElementsByTagName('tbody')[0];
+
+    // 行数を調整
+    while (table.rows.length < data.length) {
+        table.insertRow();
+    }
+    while (table.rows.length > data.length) {
+        table.deleteRow(-1);
+    }
+
+    // 行を上から書き換え
+    var position = 1;
+    data.forEach((item, index) => {
+        const row = table.rows[index];
+        let cell1 = row.cells[0];
+        let cell2 = row.cells[1];
+        let cell3 = row.cells[2];
+        if (!cell1) cell1 = row.insertCell(0);
+        if (!cell2) cell2 = row.insertCell(1);
+        if (!cell3) cell3 = row.insertCell(2);
+        cell1.innerHTML = `#${position}`;
+        cell1.classList.add('right-align');
+        cell2.innerHTML = item.name
+        cell3.innerHTML = item.count.toLocaleString();
+        cell3.classList.add('right-align');
+        position++;
+    });
+
+    // 現在のデータを保存
+    previousItemData = data;
+}
+
+async function fetchData() {
+    await fetchItem();
+}
+
+window.onload = function () {
+    fetchData();
+    setInterval(fetchData, 1000); // 1秒毎にfetchDataを実行
+};
+
+// Resizable table container
+const resizable = document.querySelector('.resizable');
+const resizer = document.querySelector('.resizer');
+
+resizer.addEventListener('mousedown', function (e) {
+    document.addEventListener('mousemove', resize);
+    document.addEventListener('mouseup', stopResize);
+});
+
+function resize(e) {
+    resizable.style.height = `${e.clientY - resizable.offsetTop}px`;
+}
+
+function stopResize() {
+    document.removeEventListener('mousemove', resize);
+    document.removeEventListener('mouseup', stopResize);
+}
---
+++ html/locale.js
@@ -0,0 +1,86 @@
+
+const localeColour = {
+    ja : {
+        white: "白",
+        orange: "橙",
+        magenta: "赤紫",
+        light_blue: "空",
+        yellow: "黄",
+        lime: "黄緑",
+        pink: "桃",
+        gray: "灰",
+        light_gray: "薄灰",
+        cyan: "青緑",
+        purple: "紫",
+        blue: "青",
+        brown: "茶",
+        green: "緑",
+        red: "赤",
+        black: "黒"
+    },
+    en : {
+        white: "White",
+        orange: "Orange",
+        magenta: "Magenta",
+        light_blue: "Light Blue",
+        yellow: "Yellow",
+        lime: "Lime",
+        pink: "Pink",
+        gray: "Gray",
+        light_gray: "Light Gray",
+        cyan: "Cyan",
+        purple: "Purple",
+        blue: "Blue",
+        brown: "Brown",
+        green: "Green",
+        red: "Red",
+        black: "Black"
+    }
+};
+
+
+const localeText = {
+    ja: {
+        itemInfoTitle: "アイテム情報",
+        itemDetailInfoTitle: "チャンネル内アイテム情報",
+        channelHeader: "チャンネル",
+        queueHeader: "待ちキュー数",
+        fluidInfoTitle: "液体情報",
+        fluidChannelHeader: "チャンネル",
+        fluidNameHeader: "液体名",
+        fluidAmountHeader: "液体量",
+        queueIdHeader: "順序番号",
+        nameHeader: "アイテムID",
+        amountHeader: "量"
+    },
+    en: {
+        itemInfoTitle: "Item Information",
+        itemDetailInfoTitle: "Item Detail Infomation",
+        channelHeader: "Channel",
+        queueHeader: "Queue Size",
+        fluidInfoTitle: "Fluid Information",
+        fluidChannelHeader: "Channel",
+        fluidNameHeader: "Fluid Name",
+        fluidAmountHeader: "Fluid Amount",
+        queueIdHeader: "Queue num",
+        nameHeader: "Item ID",
+        amountHeader: "Amount"
+    }
+};
+
+
+const locale = new URLSearchParams(window.location.search).get('lang') || 'en';
+
+function switchLanguage() {
+    const url = new URL(window.location.href);
+    const newLang = locale === 'ja' ? 'en' : 'ja';
+    url.searchParams.set('lang', newLang);
+    window.location.href = url.toString();
+}
+
+
+function ReturnToHome() {
+    const url = new URL(window.location.href);
+    const lang = url.searchParams.get('lang') || 'en';
+    window.location.href = `/?lang=${lang}`;
+}
