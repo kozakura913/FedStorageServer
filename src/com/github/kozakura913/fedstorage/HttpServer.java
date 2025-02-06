db8cc50d9b5b819cad7724f5c319b59637d1da63
Aoi Kagase <datumoujidai@gmail.com>
2025-02-06 18:38:04 +0900

    Endpointのリスト化

--- src/com/github/kozakura913/fedstorage/HttpServer.java
+++ src/com/github/kozakura913/fedstorage/HttpServer.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,87 +24,112 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 public class HttpServer extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-	private static final HashSet<String> files=new HashSet<>();
+	private static final HashSet<String> files = new HashSet<>();
+
+	private static final String endpoints[][] = {
+		{"/api/list/item_frequency.json", "item_frequency"},
+		{"/api/list/items.json", "items"},
+		{"/api/list/fluid_frequency.json", "fluid_frequency"},
+		{"/api/list/fluids.json", "fluids"},
+	};
+
+	private static final String contentTypes[][] = {
+		{".html", "text/html"},
+		{".css",  "text/css"},
+		{".js",  "text/javascript"},
+		{".png", "text/png"},
+		{".svg", "text/svg+xml"},
+	};
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String path=request.getPathInfo();
-		if(path.startsWith("/api")) {
+
+		String path = request.getPathInfo();
+
+		if (path.startsWith("/api")) {
 			response.setHeader("Content-Type","application/json");
 			PrintWriter w = response.getWriter();
-			if(path.equals("/api/list/item_frequency.json")) {
-				item_frequency(w);
-			}else if(path.equals("/api/list/items.json")) {
-				items(w,request);
-			}else if(path.equals("/api/list/fluid_frequency.json")) {
-				fluid_frequency(w);
-			}else if(path.equals("/api/list/fluids.json")) {
-				fluids(w,request);
+			Method method;
+
+			for (String[] endpoint : endpoints) {
+				if (path.equals(endpoint[0])) {
+					method = this.getClass().getMethod(endpoint[1], new Class[]{Writer.class, HttpServletRequest.class});
+					method.invoke(w, request);
+					break;
+				}
 			}
+
 			w.flush();
 			return;
 		}
-		if(path.endsWith(".html")) {
-			response.setHeader("Content-Type","text/html");
-		}else if(path.endsWith(".css")) {
-			response.setHeader("Content-Type","text/css");
-		}else if(path.endsWith(".js")) {
-			response.setHeader("Content-Type","text/javascript");
-		}else if(path.endsWith(".png")) {
-			response.setHeader("Content-Type","image/png");
-		}else if(path.endsWith(".svg")) {
-			response.setHeader("Content-Type","image/svg+xml");
+
+		for (String[] contentType : contentTypes) {
+			if (path.endsWith(contentType[0])) {
+				response.setHeader("Content-Type", contentType[1]);
+			}
 		}
-		path=path.substring(1);
-		if(path.contains("/")||path.startsWith(".")) {
+
+		path = path.substring(1);
+		if (path.contains("/") || path.startsWith(".")) {
 			response.setStatus(404);
 		}
-		if(path.isBlank())path="index.html";
-		if(files.contains(path)) {
-			URL res = HttpServer.class.getClassLoader().getResource("com/github/kozakura913/fedstorage/html/"+path);
-			if(res==null) {
+
+		if (path.isBlank())
+			path = "index.html";
+
+		if (files.contains(path)) {
+			URL res = HttpServer.class.getClassLoader().getResource("com/github/kozakura913/fedstorage/html/" + path);
+			if (res == null) {
 				response.setStatus(404);
-			}else {
-				try(InputStream is=res.openStream()){
+			} else {
+				try (InputStream is = res.openStream()) {
 					is.transferTo(response.getOutputStream());
 				}
 			}
-		}else {
+		} else {
 			response.setStatus(404);
 		}
 	}
+
+	@SuppressWarnings("unused")
 	private void fluids(PrintWriter w, HttpServletRequest req) {
 		String freq=req.getParameter("frequency");
-		if(freq==null) {
+		if (freq == null) {
 			w.append("[]");
 			return;
 		}
-		freq=freq.toUpperCase();
+		freq = freq.toUpperCase();
+
 		HashMap<String, FluidStack> freq_buffer = FedStorageServer.fluid_buffers.get(freq);
-		if(freq_buffer==null) {
+		if (freq_buffer == null) {
 			w.append("[]");
 			return;
 		}
-		ArrayList<FluidStack> copy=new ArrayList<>();
+
+		ArrayList<FluidStack> copy = new ArrayList<>();
 		synchronized(freq_buffer) {
 			copy.addAll(freq_buffer.values());
 		}
 		w.append("[\n");
-		boolean first=true;
-		for(FluidStack fs:copy) {
-			if(!first) {
+
+		boolean first = true;
+		for (FluidStack fs:copy) {
+			if (!first) {
 				w.append(",\n");
 			}else {
-				first=false;
+				first = false;
 			}
+
 			StringBuffer nbt = new StringBuffer();
-			if(fs.nbt==null) {
+			if (fs.nbt == null) {
 				nbt.append("null");
 			}else {
 				nbt.append("\"");
-				for (int j=0; j < fs.nbt.length; j++){
+				for (int j = 0; j < fs.nbt.length; j++){
 					int i = (int)fs.nbt[j] & 0xff;
-					if (i<=15){
+					if (i <= 15){
 						nbt.append("0");
 					}
 					nbt.append(Integer.toHexString(i));
@@ -114,50 +140,54 @@ public class HttpServer extends HttpServlet {
 		}
 		w.append("\n]");
 	}
-	private void fluid_frequency(Writer w) throws IOException {
+
+	@SuppressWarnings("unused")
+	private void fluid_frequency(Writer w, HttpServletRequest req) throws IOException {
 		class FreqEntry{
 			String id;
 			int size;
 		}
-		ArrayList<FreqEntry> copy=new ArrayList<>();
+		ArrayList<FreqEntry> copy = new ArrayList<>();
 		synchronized(FedStorageServer.fluid_buffers) {
 			for(Entry<String, HashMap<String, FluidStack>> e:FedStorageServer.fluid_buffers.entrySet()) {
 				FreqEntry fe = new FreqEntry();
-				fe.id=e.getKey();
-				fe.size=e.getValue().size();
+				fe.id = e.getKey();
+				fe.size = e.getValue().size();
 				copy.add(fe);
 			}
 		}
 		w.append("[\n");
-		boolean first=true;
+		boolean first = true;
 		for(FreqEntry fe:copy) {
-			if(!first) {
+			if (!first) {
 				w.append(",\n");
 			}else {
-				first=false;
+				first = false;
 			}
 			w.append("{\"id\":\"").append(fe.id).append("\",\"size\":").append(Integer.toString(fe.size)).append("}");
 		}
 		w.append("\n]");
 	}
-	private void item_frequency(Writer w) throws IOException {
+
+	@SuppressWarnings("unused")
+	private void item_frequency(Writer w, HttpServletRequest req) throws IOException {
 		class FreqEntry{
 			String id;
 			int size;
 		}
-		ArrayList<FreqEntry> copy=new ArrayList<>();
+		ArrayList<FreqEntry> copy = new ArrayList<>();
 		synchronized(FedStorageServer.item_buffers) {
 			for(Entry<String, ArrayList<ItemStack>> e:FedStorageServer.item_buffers.entrySet()) {
 				FreqEntry fe = new FreqEntry();
-				fe.id=e.getKey();
-				fe.size=e.getValue().size();
+				fe.id = e.getKey();
+				fe.size = e.getValue().size();
 				copy.add(fe);
 			}
 		}
 		w.append("[\n");
-		boolean first=true;
+		boolean first = true;
 		for(FreqEntry fe:copy) {
-			if(!first) {
+			if (!first) {
 				w.append(",\n");
 			}else {
 				first=false;
@@ -166,38 +196,40 @@ public class HttpServer extends HttpServlet {
 		}
 		w.append("\n]");
 	}
+
+	@SuppressWarnings("unused")
 	private void items(Writer w,HttpServletRequest req) throws IOException {
-		String freq=req.getParameter("frequency");
-		if(freq==null) {
+		String freq = req.getParameter("frequency");
+		if (freq == null) {
 			w.append("[]");
 			return;
 		}
-		freq=freq.toUpperCase();
+		freq = freq.toUpperCase();
 		ArrayList<ItemStack> freq_buffer = FedStorageServer.item_buffers.get(freq);
-		if(freq_buffer==null) {
+		if (freq_buffer == null) {
 			w.append("[]");
 			return;
 		}
 		ArrayList<ItemStack> copy;
 		synchronized(freq_buffer) {
-			copy=(ArrayList<ItemStack>) freq_buffer.clone();
+			copy = new ArrayList<ItemStack>(freq_buffer);
 		}
 		w.append("[\n");
-		boolean first=true;
+		boolean first = true;
 		for(ItemStack is:copy) {
-			if(!first) {
+			if (!first) {
 				w.append(",\n");
 			}else {
-				first=false;
+				first = false;
 			}
 			StringBuffer nbt = new StringBuffer();
-			if(is.nbt==null) {
+			if (is.nbt == null) {
 				nbt.append("null");
 			}else {
 				nbt.append("\"");
-				for (int j=0; j < is.nbt.length; j++){
+				for (int j = 0; j < is.nbt.length; j++){
 					int i = (int)is.nbt[j] & 0xff;
-					if (i<=15){
+					if (i <= 15){
 						nbt.append("0");
 					}
 					nbt.append(Integer.toHexString(i));
@@ -208,9 +240,10 @@ public class HttpServer extends HttpServlet {
 		}
 		w.append("\n]");
 	}
+
 	public static void start() {
 		String[] src = new File("src/com/github/kozakura913/fedstorage/html").list();
-		if(src!=null&&src.length>0) {
+		if (src != null && src.length > 0) {
 			try (FileOutputStream list_txt = new FileOutputStream("src/com/github/kozakura913/fedstorage/html/list.txt")){
 				for(String file:src) {
 					list_txt.write((file+"\n").getBytes("UTF-8"));
@@ -220,14 +253,14 @@ public class HttpServer extends HttpServlet {
 			}
 		}
 		URL res = HttpServer.class.getClassLoader().getResource("com/github/kozakura913/fedstorage/html/list.txt");
-		if(res==null) {
+		if (res == null) {
 			System.out.println("FrontendResource notfound");
 		}else {
 			try(InputStream is = res.openStream()){
 				BufferedReader br = new BufferedReader(new InputStreamReader(is));
 				while(true) {
-					String line=br.readLine();
-					if(line==null)break;
+					String line = br.readLine();
+					if (line == null)break;
 					files.add(line);
 				}
 			} catch (IOException e) {
