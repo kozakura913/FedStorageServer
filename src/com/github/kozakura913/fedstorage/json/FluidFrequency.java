5c55b14d7977c178681472b55ed75e181f55c882
Aoi Kagase <datumoujidai@gmail.com>
2025-02-09 00:32:06 +0900

    JSON構築処理の修正

---
+++ src/com/github/kozakura913/fedstorage/json/FluidFrequency.java
@@ -0,0 +1,22 @@
+package com.github.kozakura913.fedstorage.json;
+
+public class FluidFrequency {
+    private String id;
+    private int size; 
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
